e18fceae42746f2e88e747c5402f0acd819f67d9
Eonji-sw <ejkim0625@gmail.com>
2024-04-02 21:04:02 +0900

    [SETTING] : Project basic setting

---
+++ app/src/main/java/com/sopt/now/core/base/BindingActivity.kt
@@ -0,0 +1,30 @@
+package com.sopt.now.core.base
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.sopt.now.core.util.context.hideKeyboard
+
+abstract class BindingActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+        initView()
+    }
+
+    protected abstract fun initView()
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboard(currentFocus ?: View(this))
+        return super.dispatchTouchEvent(ev)
+    }
+}
