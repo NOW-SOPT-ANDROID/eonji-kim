8a963c980fb4d61280989106f9f8772d71185994
Eonji-sw <ejkim0625@gmail.com>
2024-04-04 14:06:19 +0900

    [SETTING] : basic color setting

--- app/src/main/java/com/sopt/now/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/now/compose/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = YellowSub,
+    secondary = GreenSub,
+    tertiary = Blue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = YellowMain,
+    secondary = GreenMain,
+    tertiary = Blue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
