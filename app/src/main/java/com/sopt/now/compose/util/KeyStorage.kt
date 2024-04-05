7ec532883f6148933312738df601af062f9478ed
Eonji-sw <ejkim0625@gmail.com>
2024-04-05 09:52:26 +0900

    [FEAT] : KeyStorage 생성 및 상수 분리

---
+++ app/src/main/java/com/sopt/now/compose/util/KeyStorage.kt
@@ -0,0 +1,8 @@
+package com.sopt.now.compose.util
+
+object KeyStorage {
+    const val USER_ID = "id"
+    const val USER_PW = "pw"
+    const val USER_NICKNAME = "nickname"
+    const val USER_AGE = "age"
+}
