60c8a3d69a60353010317ca11a5cfb0d21966936
Eonji-sw <ejkim0625@gmail.com>
2024-04-05 09:26:37 +0900

    [HOTFIX] : null safety 처리

--- app/src/main/java/com/sopt/now/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/MainActivity.kt
@@ -3,6 +3,10 @@ package com.sopt.now.presentation
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
 import com.sopt.now.databinding.ActivityMainBinding
+import com.sopt.now.presentation.util.KeyStorage.USER_AGE
+import com.sopt.now.presentation.util.KeyStorage.USER_ID
+import com.sopt.now.presentation.util.KeyStorage.USER_NICKNAME
+import com.sopt.now.presentation.util.KeyStorage.USER_PW
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
@@ -11,10 +15,10 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     private fun initResultLoginUserInformation() {
         with(binding) {
-            tvMainNicknameValue.text = intent.getStringExtra("nickname")!!
-            tvMainAgeValue.text = intent.getStringExtra("age")!!
-            tvMainIdValue.text = intent.getStringExtra("id")!!
-            tvMainPwValue.text = intent.getStringExtra("pw")!!
+            tvMainNicknameValue.text = intent?.getStringExtra(USER_NICKNAME)
+            tvMainAgeValue.text = intent?.getStringExtra(USER_AGE)
+            tvMainIdValue.text = intent?.getStringExtra(USER_ID)
+            tvMainPwValue.text = intent?.getStringExtra(USER_PW)
         }
     }
 }
