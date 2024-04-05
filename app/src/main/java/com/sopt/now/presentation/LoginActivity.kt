fdf0ffdf04fbff9ef45026a5cbc4d43a2d984282
Eonji-sw <ejkim0625@gmail.com>
2024-04-05 20:46:53 +0900

    [FEAT] : intent flag 추가

--- app/src/main/java/com/sopt/now/presentation/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/LoginActivity.kt
@@ -70,6 +70,7 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
                     USER_AGE to userAge
                 )
             )
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         startActivity(intent)
     }
