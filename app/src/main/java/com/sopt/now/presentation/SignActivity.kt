e1a535a30f8f0b43ceaa3822ed35a630ec546b43
Eonji-sw <ejkim0625@gmail.com>
2024-04-05 09:27:04 +0900

    [MOD] : 상수 분리 및 함수명 변경

--- app/src/main/java/com/sopt/now/presentation/SignActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/SignActivity.kt
@@ -6,13 +6,17 @@ import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
 import com.sopt.now.core.util.context.snackBar
 import com.sopt.now.databinding.ActivitySignBinding
+import com.sopt.now.presentation.util.KeyStorage.USER_AGE
+import com.sopt.now.presentation.util.KeyStorage.USER_ID
+import com.sopt.now.presentation.util.KeyStorage.USER_NICKNAME
+import com.sopt.now.presentation.util.KeyStorage.USER_PW
 
 class SignActivity : BindingActivity<ActivitySignBinding>(R.layout.activity_sign) {
     override fun initView() {
         initSignBtnClickListener()
     }
 
-    private fun initSignCheck() {
+    private fun checkSignValid() {
         with(binding) {
             when {
                 etSignId.text.trim().length !in 6..10 -> snackBar(root) { getString(R.string.error_message_sign_invalid_id) }
@@ -22,19 +26,24 @@ class SignActivity : BindingActivity<ActivitySignBinding>(R.layout.activity_sign
 
                 etSignAge.text.trim().length !in 1..3 -> snackBar(root) { getString(R.string.error_message_sign_invalid_age) }
 
-                else -> navigateToLogin()
+                else -> navigateToLogin(
+                    etSignId.text.toString(),
+                    etSignPw.text.toString(),
+                    etSignNickname.text.toString(),
+                    etSignAge.text.toString()
+                )
             }
         }
     }
 
-    private fun ActivitySignBinding.navigateToLogin() {
+    private fun navigateToLogin(id: String, pw: String, nickname: String, age: String) {
         val intent = Intent(this@SignActivity, LoginActivity::class.java).apply {
             putExtras(
                 bundleOf(
-                    "id" to etSignId.text.toString(),
-                    "pw" to etSignPw.text.toString(),
-                    "nickname" to etSignNickname.text.toString(),
-                    "age" to etSignAge.text.toString()
+                    USER_ID to id,
+                    USER_PW to pw,
+                    USER_NICKNAME to nickname,
+                    USER_AGE to age
                 )
             )
         }
@@ -44,7 +53,7 @@ class SignActivity : BindingActivity<ActivitySignBinding>(R.layout.activity_sign
 
     private fun initSignBtnClickListener() {
         binding.btnSignSign.setOnClickListener {
-            initSignCheck()
+            checkSignValid()
         }
     }
 }
