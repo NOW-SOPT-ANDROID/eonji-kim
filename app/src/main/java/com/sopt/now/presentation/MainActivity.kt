0210bd5e20e27ff3329a1e62d4a05a75dfd1ae10
Eonji-sw <ejkim0625@gmail.com>
2024-04-05 20:47:22 +0900

    [FEAT] : 뒤로가기 두 번 클릭시 앱 종료 구현

--- app/src/main/java/com/sopt/now/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sopt.now.presentation
 
+import androidx.activity.OnBackPressedCallback
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
+import com.sopt.now.core.util.context.toast
 import com.sopt.now.databinding.ActivityMainBinding
 import com.sopt.now.presentation.util.KeyStorage.USER_AGE
 import com.sopt.now.presentation.util.KeyStorage.USER_ID
@@ -9,8 +11,23 @@ import com.sopt.now.presentation.util.KeyStorage.USER_NICKNAME
 import com.sopt.now.presentation.util.KeyStorage.USER_PW
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private var backPressedTime = 0L
+
     override fun initView() {
         initResultLoginUserInformation()
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime <= 2000) {
+                finish()
+            } else {
+                backPressedTime = System.currentTimeMillis()
+                toast("종료하시려면 한 번 더 눌러주세요")
+            }
+        }
     }
 
     private fun initResultLoginUserInformation() {
