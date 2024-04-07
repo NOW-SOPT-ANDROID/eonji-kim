0c9d4f36eacb055358f61084373e746b851d5094
Eonji-sw <ejkim0625@gmail.com>
2024-04-08 04:26:04 +0900

    [MOD] : 상수화 추가

--- app/src/main/java/com/sopt/now/presentation/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/now/presentation/util/KeyStorage.kt
@@ -1,8 +1,10 @@
 package com.sopt.now.presentation.util
 
 object KeyStorage {
-    const val USER_ID = "id"
-    const val USER_PW = "pw"
-    const val USER_NICKNAME = "nickname"
-    const val USER_AGE = "age"
+    const val USER_DATA = "user_data"
+    const val ERROR_LOGIN_ID_PW = "error_login_id_pw"
+    const val ERROR_SIGN_ID = "error_sign_id"
+    const val ERROR_SIGN_PW = "error_sign_pw"
+    const val ERROR_SIGN_NICKNAME = "error_sign_nickname"
+    const val ERROR_SIGN_AGE = "error_sign_age"
 }
