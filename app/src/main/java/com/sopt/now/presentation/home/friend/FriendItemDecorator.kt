7a9bea5c52593f65e8e60892b03ba0584524eda0
Eonji-sw <ejkim0625@gmail.com>
2024-04-15 19:46:22 +0900

    [FEAT] : friend item decorator 구현

---
+++ app/src/main/java/com/sopt/now/presentation/home/friend/FriendItemDecorator.kt
@@ -0,0 +1,26 @@
+package com.sopt.now.presentation.home.friend
+
+import android.content.Context
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.now.core.util.context.pxToDp
+
+class FriendItemDecorator(private val context: Context) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State,
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        val position = parent.getChildAdapterPosition(view)
+
+        if (position == 0) {
+            outRect.top = context.pxToDp(8)
+        } else {
+            outRect.bottom = context.pxToDp(2)
+        }
+        outRect.bottom = context.pxToDp(8)
+    }
+}
