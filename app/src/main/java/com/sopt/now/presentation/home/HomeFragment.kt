d139db50802583de589444a4fe39b50a570f5750
Eonji-sw <ejkim0625@gmail.com>
2024-04-15 20:39:26 +0900

    [MOD] : 함수 처리 및 concatAdapter 구현

--- app/src/main/java/com/sopt/now/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/home/HomeFragment.kt
@@ -1,23 +1,30 @@
 package com.sopt.now.presentation.home
 
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingFragment
 import com.sopt.now.databinding.FragmentHomeBinding
 import com.sopt.now.presentation.home.friend.FriendAdapter
 import com.sopt.now.presentation.home.friend.FriendItemDecorator
+import com.sopt.now.presentation.home.user.UserAdapter
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun initView() {
-        binding.rvHomeFriends.adapter = FriendAdapter().apply {
-            submitList(homeViewModel.mockFriendList)
-        }
+        initHomeAdapter()
+    }
+
+    private fun initHomeAdapter() {
+        val userAdapter = UserAdapter().apply { submitList(listOf(homeViewModel.getUserInfo())) }
+        val friendAdapter = FriendAdapter().apply { submitList(homeViewModel.mockFriendList) }
+
         binding.rvHomeFriends.run {
+            adapter = ConcatAdapter(userAdapter, friendAdapter)
             layoutManager = LinearLayoutManager(requireContext())
-            addItemDecoration(FriendItemDecorator(requireContext()))
+            addItemDecoration(FriendItemDecorator(context))
         }
     }
 }
