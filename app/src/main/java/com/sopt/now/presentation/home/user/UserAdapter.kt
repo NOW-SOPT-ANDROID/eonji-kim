4963bac80fb8b30b0e207cdb687e86691dfb16f3
Eonji-sw <ejkim0625@gmail.com>
2024-04-15 20:38:22 +0900

    [FEAT] : user adapter 구현

---
+++ app/src/main/java/com/sopt/now/presentation/home/user/UserAdapter.kt
@@ -0,0 +1,34 @@
+package com.sopt.now.presentation.home.user
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.sopt.now.core.util.view.ItemDiffCallback
+import com.sopt.now.databinding.ItemUserBinding
+import com.sopt.now.presentation.model.User
+
+class UserAdapter() : ListAdapter<User, UserViewHolder>(UserAdapterDiffCallback) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UserViewHolder {
+        val binding =
+            ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: UserViewHolder,
+        position: Int
+    ) {
+        holder.onBind(currentList[position])
+    }
+
+    companion object {
+        private val UserAdapterDiffCallback =
+            ItemDiffCallback<User>(
+                onItemsTheSame = { old, new -> old.id == new.id },
+                onContentsTheSame = { old, new -> old == new }
+            )
+    }
+}
