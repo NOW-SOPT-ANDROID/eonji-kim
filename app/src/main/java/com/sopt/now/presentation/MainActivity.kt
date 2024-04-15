aac38d73c677399dd5ce395732a4636ec4c329f3
Eonji-sw <ejkim0625@gmail.com>
2024-04-15 19:37:45 +0900

    [MOD] : mainActivity -> myPageFrament로 코드 move

--- app/src/main/java/com/sopt/now/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/MainActivity.kt
@@ -1,42 +1,13 @@
 package com.sopt.now.presentation
 
-import androidx.activity.OnBackPressedCallback
+import android.content.Intent
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
-import com.sopt.now.core.util.context.toast
 import com.sopt.now.databinding.ActivityMainBinding
-import com.sopt.now.presentation.model.User
-import com.sopt.now.presentation.util.KeyStorage.USER_DATA
+import com.sopt.now.presentation.login.LoginActivity
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private var userData: User? = null
-    private var backPressedTime = 0L
-
     override fun initView() {
-        initResultLoginUserInformation()
-
-        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
-    }
-
-    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            if (System.currentTimeMillis() - backPressedTime <= 2000) {
-                finish()
-            } else {
-                backPressedTime = System.currentTimeMillis()
-                toast(getString(R.string.toast_main_back))
-            }
-        }
-    }
-
-    private fun initResultLoginUserInformation() {
-        with(binding) {
-            userData = intent?.getParcelableExtra(USER_DATA)
-
-            tvMainNicknameValue.text = userData?.nickname
-            tvMainAgeValue.text = userData?.age + "세"
-            tvMainIdValue.text = userData?.id
-            tvMainPwValue.text = userData?.pw
-        }
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
