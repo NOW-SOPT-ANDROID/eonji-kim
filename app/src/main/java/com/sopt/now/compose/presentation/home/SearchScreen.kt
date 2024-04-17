5b7ac796c8a3f5387b062d2fb3565bdd6493e4ba
Eonji-sw <ejkim0625@gmail.com>
2024-04-17 15:28:27 +0900

    [UI] : search screen 구현

---
+++ app/src/main/java/com/sopt/now/compose/presentation/home/SearchScreen.kt
@@ -0,0 +1,27 @@
+package com.sopt.now.compose.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text("search")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchScreenPreview() {
+    NOWSOPTAndroidTheme {
+        SearchScreen()
+    }
+}
