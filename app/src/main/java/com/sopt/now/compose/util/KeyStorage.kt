5619da4094652a241d02c5f5a980e8d7787dec97
Eonji-sw <ejkim0625@gmail.com>
2024-04-17 15:22:48 +0900

    [FEAT] : user key 상수화

--- app/src/main/java/com/sopt/now/compose/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/now/compose/util/KeyStorage.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.compose.util
 
 object KeyStorage {
+    const val USER_DATA = "user"
     const val USER_ID = "id"
     const val USER_PW = "pw"
     const val USER_NICKNAME = "nickname"
