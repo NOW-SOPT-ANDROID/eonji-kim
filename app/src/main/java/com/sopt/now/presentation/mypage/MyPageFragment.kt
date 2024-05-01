537b14a749cd6c8fb00fd8aa2d813e3ce6993929
Eonji-sw <ejkim0625@gmail.com>
2024-05-01 17:07:55 +0900

    [MOD] : myPage view model 적용

--- app/src/main/java/com/sopt/now/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/mypage/MyPageFragment.kt
@@ -3,22 +3,39 @@ package com.sopt.now.presentation.mypage
 import androidx.fragment.app.viewModels
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingFragment
+import com.sopt.now.core.util.fragment.snackBar
+import com.sopt.now.core.util.view.UiState
+import com.sopt.now.data.dto.response.ResponseGetUserInfoDataDto
 import com.sopt.now.databinding.FragmentMyPageBinding
-import com.sopt.now.presentation.login.LoginViewModel
+import timber.log.Timber
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-    private val loginViewModel by viewModels<LoginViewModel>()
+    private val myPageViewModel by viewModels<MyPageViewModel>()
 
     override fun initView() {
-        initResultLoginUserInformation()
+        initObserveUserInfo()
     }
 
-    private fun initResultLoginUserInformation() {
+    private fun initObserveUserInfo() {
+        myPageViewModel.getUserInfo.observe(this) {
+            when (it) {
+                is UiState.Success -> initResultLoginUserInformation(it.data.data)
+                is UiState.Failure -> initErrorMessage(it.errorMessage)
+                is UiState.Loading -> Timber.d("로딩중")
+            }
+        }
+    }
+
+    private fun initErrorMessage(errorMessage: String) {
+        snackBar(binding.root) { "유저조회 실패 : $errorMessage" }
+    }
+
+    private fun initResultLoginUserInformation(data: ResponseGetUserInfoDataDto) {
         with(binding) {
-            tvMyPageNicknameValue.text = loginViewModel.getUserInfo().nickname
-            tvMyPageAgeValue.text = "${loginViewModel.getUserInfo().age}세"
-            tvMyPageIdValue.text = loginViewModel.getUserInfo().id
-            tvMyPagePwValue.text = loginViewModel.getUserInfo().pw
+            tvMyPageMemberIdValue.text = "${myPageViewModel.getUserMemberId()}번"
+            tvMyPageNicknameValue.text = data.nickname
+            tvMyPageIdValue.text = data.authenticationId
+            tvMyPageTelValue.text = data.phone
         }
     }
 }
