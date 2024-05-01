fa3699e2ea9f5b274ed1aaaa891fdbe9af6aaae6
Eonji-sw <ejkim0625@gmail.com>
2024-05-01 17:09:25 +0900

    [MOD] : key 상수 추가 및 변경

--- app/src/main/java/com/sopt/now/presentation/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/now/presentation/util/KeyStorage.kt
@@ -1,13 +1,14 @@
 package com.sopt.now.presentation.util
 
 object KeyStorage {
+    const val MEMBER_ID = "member_id"
     const val USER_ID = "id"
     const val USER_PW = "pw"
     const val USER_NICKNAME = "nickname"
-    const val USER_AGE = "age"
+    const val USER_TEL = "tel"
     const val ERROR_LOGIN_ID_PW = "error_login_id_pw"
     const val ERROR_SIGN_ID = "error_sign_id"
     const val ERROR_SIGN_PW = "error_sign_pw"
     const val ERROR_SIGN_NICKNAME = "error_sign_nickname"
-    const val ERROR_SIGN_AGE = "error_sign_age"
+    const val ERROR_SIGN_TEL = "error_sign_tel"
 }
