475ba609b761545508345f4266f0b33aba6e68e5
Eonji-sw <ejkim0625@gmail.com>
2024-05-02 22:06:17 +0900

    [FEAT] : getUserList add

--- app/src/main/java/com/sopt/now/data/api/UserServiceApi.kt
+++ app/src/main/java/com/sopt/now/data/api/UserServiceApi.kt
@@ -1,14 +1,15 @@
 package com.sopt.now.data.api
 
-import com.sopt.now.data.KeyStorage.INFO
-import com.sopt.now.data.KeyStorage.MEMBER
-import com.sopt.now.data.dto.response.ResponseGetUserInfoDto
+import com.sopt.now.data.KeyStorage.API
+import com.sopt.now.data.KeyStorage.USERS
+import com.sopt.now.data.dto.response.ResponseUserListDto
+import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface UserServiceApi {
-    @GET("$MEMBER/$INFO")
-    suspend fun getUserInfo(
-        @Header("memberId") memberId: Int
-    ): ResponseGetUserInfoDto
+    @GET("$API/$USERS")
+    suspend fun getUserList(
+        @Query("page") page: Int
+    ): Response<ResponseUserListDto>
 }
