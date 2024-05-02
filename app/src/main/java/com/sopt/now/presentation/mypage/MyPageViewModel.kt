6f9711edcb651e6439b85f01d13210c196e3ee60
Eonji-sw <ejkim0625@gmail.com>
2024-05-02 22:10:05 +0900

    [MOD] : 인자명 변경

--- app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
@@ -6,19 +6,19 @@ import androidx.lifecycle.viewModelScope
 import com.sopt.now.core.MainApplication
 import com.sopt.now.core.util.view.UiState
 import com.sopt.now.data.ServicePool
-import com.sopt.now.data.dto.response.ResponseGetUserInfoDto
+import com.sopt.now.data.dto.response.ResponseUserInfoDto
 import kotlinx.coroutines.launch
 
 class MyPageViewModel : ViewModel() {
-    private val _getUserInfo = MutableLiveData<UiState<ResponseGetUserInfoDto>>()
-    val getUserInfo: MutableLiveData<UiState<ResponseGetUserInfoDto>> = _getUserInfo
+    private val _getUserInfo = MutableLiveData<UiState<ResponseUserInfoDto>>()
+    val getUserInfo: MutableLiveData<UiState<ResponseUserInfoDto>> = _getUserInfo
 
     init {
         getUserInfo()
     }
 
     private fun getUserInfo() = viewModelScope.launch {
-        runCatching { ServicePool.userServiceApi.getUserInfo(getUserMemberId()) }.fold(
+        runCatching { ServicePool.authServiceApi.getUserInfo(getUserMemberId()) }.fold(
             { _getUserInfo.value = UiState.Success(it) },
             { _getUserInfo.value = UiState.Failure(it.message.toString()) }
         )
