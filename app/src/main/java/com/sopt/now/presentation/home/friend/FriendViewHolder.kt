33067e43273b1cd622a025f9a816a24bd51ba866
Eonji-sw <ejkim0625@gmail.com>
2024-05-02 22:09:18 +0900

    [MOD] : 바인딩 변경

--- app/src/main/java/com/sopt/now/presentation/home/friend/FriendViewHolder.kt
+++ app/src/main/java/com/sopt/now/presentation/home/friend/FriendViewHolder.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.presentation.home.friend
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sopt.now.databinding.ItemFriendBinding
 import com.sopt.now.presentation.model.Friend
 
@@ -8,9 +9,9 @@ class FriendViewHolder(private val binding: ItemFriendBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(friendData: Friend) {
         binding.run {
-            ivFriendProfile.setImageResource(friendData.profileImage)
-            tvFriendName.text = friendData.name
-            tvFriendSelfDescription.text = friendData.selfDescription
+            Glide.with(root.context).load(friendData.avatar).into(ivFriendProfile)
+            tvFriendName.text = friendData.firstName + friendData.lastName
+            tvFriendSelfDescription.text = friendData.email
         }
     }
 }
