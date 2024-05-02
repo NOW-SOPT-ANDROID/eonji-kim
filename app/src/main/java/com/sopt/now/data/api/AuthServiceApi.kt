db02db351d5050fea5533514aa7311989fa169ba
Eonji-sw <ejkim0625@gmail.com>
2024-05-02 22:06:07 +0900

    [FEAT] : getUserInfo add

--- app/src/main/java/com/sopt/now/data/api/AuthServiceApi.kt
+++ app/src/main/java/com/sopt/now/data/api/AuthServiceApi.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.data.api
 
+import com.sopt.now.data.KeyStorage.INFO
 import com.sopt.now.data.KeyStorage.JOIN
 import com.sopt.now.data.KeyStorage.LOGIN
 import com.sopt.now.data.KeyStorage.MEMBER
@@ -7,8 +8,11 @@ import com.sopt.now.data.dto.request.RequestLoginDto
 import com.sopt.now.data.dto.request.RequestSignDto
 import com.sopt.now.data.dto.response.ResponseLoginDto
 import com.sopt.now.data.dto.response.ResponseSignDto
+import com.sopt.now.data.dto.response.ResponseUserInfoDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthServiceApi {
@@ -21,4 +25,9 @@ interface AuthServiceApi {
     suspend fun postLogin(
         @Body requestLoginDto: RequestLoginDto
     ): Response<ResponseLoginDto>
+
+    @GET("$MEMBER/${INFO}")
+    suspend fun getUserInfo(
+        @Header("memberId") memberId: Int
+    ): ResponseUserInfoDto
 }
