4c54fd33fb2c7e65223d87a8c778af7f773cc11e
Eonji-sw <ejkim0625@gmail.com>
2024-05-03 21:15:52 +0900

    [MOD] : login viewModel stateFlow 변경

--- app/src/main/java/com/sopt/now/compose/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/login/LoginViewModel.kt
@@ -1,17 +1,20 @@
 package com.sopt.now.compose.presentation.login
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.data.ServicePool
 import com.sopt.now.compose.data.dto.request.RequestLoginDto
+import com.sopt.now.compose.data.dto.response.ResponseLoginDto
 import com.sopt.now.compose.util.KeyStorage.ERROR_LOGIN_ID_PW
 import com.sopt.now.compose.util.MainApplication
+import com.sopt.now.compose.util.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
-    private val _postLogin = MutableLiveData<LoginState>()
-    val postLogin: MutableLiveData<LoginState> = _postLogin
+    private val _postLogin = MutableStateFlow<UiState<Any?>>(UiState.Loading)
+    val postLogin: StateFlow<UiState<Any?>> = _postLogin
 
     fun postLogin(id: String, pw: String) = viewModelScope.launch {
         if (checkLoginValid(id, pw)) {
@@ -20,18 +23,14 @@ class LoginViewModel : ViewModel() {
             }.fold(
                 {
                     if (it.code() == 200) {
-                        _postLogin.value =
-                            LoginState(
-                                true,
-                                "로그인 성공! 유저의 ID는 ${it.headers()["location"]} 입니둥"
-                            ).apply { setUserMemberId(it.headers()["location"]?.toInt() ?: -1) }
-                    } else _postLogin.value =
-                        LoginState(false, "${it.errorBody()?.string()?.split("\"")?.get(5)}")
+                        _postLogin.value = UiState.Success("로그인 성공! 유저의 ID는 ${it.headers()["location"]} 입니둥").apply { setUserMemberId(it.headers()["location"]?.toInt() ?: -1) }
+                    } else _postLogin.value = UiState.Failure("${it.errorBody()?.string()?.split("\"")?.get(5)}")
+
                 },
-                { _postLogin.value = LoginState(false, "${it.message}") }
+                { _postLogin.value = UiState.Failure(it.message.toString()) }
             )
         } else {
-            _postLogin.value = LoginState(false, ERROR_LOGIN_ID_PW)
+            _postLogin.value = UiState.Failure(ERROR_LOGIN_ID_PW)
         }
     }
 
