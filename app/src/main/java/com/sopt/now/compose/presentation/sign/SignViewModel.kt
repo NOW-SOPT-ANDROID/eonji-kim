795b9c1eee2892770002fde6db691a6d95df2a6f
Eonji-sw <ejkim0625@gmail.com>
2024-05-03 21:16:02 +0900

    [MOD] : sign viewModel stateFlow 변경

--- app/src/main/java/com/sopt/now/compose/presentation/sign/SignViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/sign/SignViewModel.kt
@@ -5,17 +5,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.compose.data.ServicePool
 import com.sopt.now.compose.data.dto.request.RequestSignDto
+import com.sopt.now.compose.data.dto.response.ResponseSignDto
 import com.sopt.now.compose.model.User
 import com.sopt.now.compose.util.KeyStorage.ERROR_SIGN_ID
 import com.sopt.now.compose.util.KeyStorage.ERROR_SIGN_NICKNAME
 import com.sopt.now.compose.util.KeyStorage.ERROR_SIGN_PW
 import com.sopt.now.compose.util.KeyStorage.ERROR_SIGN_TEL
+import com.sopt.now.compose.util.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
 
 class SignViewModel : ViewModel() {
-    private val _postSign = MutableLiveData<SignState>()
-    val postSign: MutableLiveData<SignState> = _postSign
+    private val _postSign = MutableStateFlow<UiState<Any?>>(UiState.Loading)
+    val postSign: StateFlow<UiState<Any?>> = _postSign
 
     fun postSign(user: User) = viewModelScope.launch {
         if (checkSignValid(user) == null) {
@@ -26,27 +30,22 @@ class SignViewModel : ViewModel() {
             }.fold(
                 {
                     if (it.code() == 201) {
-                        _postSign.value =
-                            SignState(
-                                true,
-                                "회원가입 성공! 유저의 ID는 ${it.headers()["location"]} 입니둥"
-                            )
-                    } else _postSign.value =
-                        SignState(false, "${it.errorBody()?.string()?.split("\"")?.get(5)}")
+                        _postSign.value = UiState.Success( "회원가입 성공! 유저의 ID는 ${it.headers()["location"]} 입니둥")
+                    } else _postSign.value = UiState.Failure("${it.errorBody()?.string()?.split("\"")?.get(5)}")
                 },
-                { _postSign.value = SignState(false, "${it.message}") }
+                { _postSign.value = UiState.Failure(it.message.toString()) }
             )
         } else {
-            _postSign.value = checkSignValid(user)
+            _postSign.value = UiState.Failure(checkSignValid(user).toString())
         }
     }
 
-    private fun checkSignValid(user: User): SignState? {
+    private fun checkSignValid(user: User): String? {
         return when {
-            !getSignIdValid(user.id) -> SignState(false, ERROR_SIGN_ID)
-            !getSignPwValid(user.pw) -> SignState(false, ERROR_SIGN_PW)
-            !getSignNicknameValid(user.nickname) -> SignState(false, ERROR_SIGN_NICKNAME)
-            !getSignTelValid(user.tel) -> SignState(false, ERROR_SIGN_TEL)
+            !getSignIdValid(user.id) -> ERROR_SIGN_ID
+            !getSignPwValid(user.pw) -> ERROR_SIGN_PW
+            !getSignNicknameValid(user.nickname) -> ERROR_SIGN_NICKNAME
+            !getSignTelValid(user.tel) -> ERROR_SIGN_TEL
             else -> null
         }
     }
