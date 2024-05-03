0c68c16501fba3258275e17897f8c9f94c394566
Eonji-sw <ejkim0625@gmail.com>
2024-05-03 19:45:15 +0900

    [MOD] : user data class 수정

--- app/src/main/java/com/sopt/now/compose/model/User.kt
+++ app/src/main/java/com/sopt/now/compose/model/User.kt
@@ -3,10 +3,9 @@ package com.sopt.now.compose.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class User(
     val id: String,
     val pw: String,
     val nickname: String,
-    val age: String,
-) : Parcelable
+    val tel: String,
+)
