8c89450da2bf9b7c28cbd35df024ee08c380b50f
Eonji-sw <ejkim0625@gmail.com>
2024-05-08 16:47:34 +0900

    [MOD] : 불필요한 코드 제거

--- app/src/main/java/com/sopt/now/data/PreferenceUtil.kt
+++ app/src/main/java/com/sopt/now/data/PreferenceUtil.kt
@@ -8,12 +8,8 @@ class PreferenceUtil(context: Context) {
     private val prefs: SharedPreferences =
         context.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
 
-    private fun getString(key: String, defValue: String): String =
-        prefs.getString(key, defValue).toString()
-
     private fun getInt(key: String, defValue: Int): Int = prefs.getInt(key, defValue)
 
-    private fun setString(key: String, str: String) = prefs.edit().putString(key, str).apply()
     private fun setInt(key: String, value: Int) = prefs.edit().putInt(key, value).apply()
 
     fun getUser() = getInt(MEMBER_ID, -1)
