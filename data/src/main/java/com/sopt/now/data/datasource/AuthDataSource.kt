3fd68eab3b6e87e8f8a6295e2d18a2252169ed05
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 03:27:19 +0900

    [MOD] : 코드 정리

--- data/src/main/java/com/sopt/now/data/datasource/AuthDataSource.kt
+++ data/src/main/java/com/sopt/now/data/datasource/AuthDataSource.kt
@@ -2,11 +2,8 @@ package com.sopt.now.data.datasource
 
 import com.sopt.now.data.dto.request.RequestLoginDto
 import com.sopt.now.data.dto.request.RequestSignDto
-import com.sopt.now.data.dto.request.RequestUserPwDto
 import com.sopt.now.data.dto.response.ResponseLoginDto
 import com.sopt.now.data.dto.response.ResponseSignDto
-import com.sopt.now.data.dto.response.ResponseUserInfoDto
-import com.sopt.now.data.dto.response.ResponseUserPwDto
 import retrofit2.Response
 
 interface AuthDataSource {
