514a08b0b87ada3b0f719035f893ee46b8cf8d89
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 02:49:47 +0900

    [FEAT] : data-remote 모듈 api service 구현(이동)

--- data-remote/src/main/java/com/sopt/now/data_remote/api/AuthApiService.kt
+++ data-remote/src/main/java/com/sopt/now/data_remote/api/AuthApiService.kt
@@ -1,11 +1,5 @@
-package com.sopt.now.data.api
+package com.sopt.now.data_remote.api
 
-import com.sopt.now.data.KeyStorage.INFO
-import com.sopt.now.data.KeyStorage.JOIN
-import com.sopt.now.data.KeyStorage.LOGIN
-import com.sopt.now.data.KeyStorage.MEMBER
-import com.sopt.now.data.KeyStorage.MEMBER_ID
-import com.sopt.now.data.KeyStorage.PASSWORD
 import com.sopt.now.data.dto.request.RequestLoginDto
 import com.sopt.now.data.dto.request.RequestSignDto
 import com.sopt.now.data.dto.request.RequestUserPwDto
@@ -13,6 +7,12 @@ import com.sopt.now.data.dto.response.ResponseLoginDto
 import com.sopt.now.data.dto.response.ResponseSignDto
 import com.sopt.now.data.dto.response.ResponseUserInfoDto
 import com.sopt.now.data.dto.response.ResponseUserPwDto
+import com.sopt.now.data_remote.ApiKeyStorage.INFO
+import com.sopt.now.data_remote.ApiKeyStorage.JOIN
+import com.sopt.now.data_remote.ApiKeyStorage.LOGIN
+import com.sopt.now.data_remote.ApiKeyStorage.MEMBER
+import com.sopt.now.data_remote.ApiKeyStorage.MEMBER_ID
+import com.sopt.now.data_remote.ApiKeyStorage.PASSWORD
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -20,7 +20,7 @@ import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 
-interface AuthServiceApi {
+interface AuthApiService {
     @POST("$MEMBER/$JOIN")
     suspend fun postSign(
         @Body requestSignDto: RequestSignDto,
--- data-remote/src/main/java/com/sopt/now/data_remote/api/ReqresApiService.kt
+++ data-remote/src/main/java/com/sopt/now/data_remote/api/ReqresApiService.kt
@@ -1,14 +1,14 @@
-package com.sopt.now.data.api
+package com.sopt.now.data_remote.api
 
-import com.sopt.now.data.KeyStorage.API
-import com.sopt.now.data.KeyStorage.PAGE
-import com.sopt.now.data.KeyStorage.USERS
 import com.sopt.now.data.dto.response.ResponseUserListDto
+import com.sopt.now.data_remote.ApiKeyStorage.API
+import com.sopt.now.data_remote.ApiKeyStorage.PAGE
+import com.sopt.now.data_remote.ApiKeyStorage.USERS
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface UserServiceApi {
+interface ReqresApiService {
     @GET("$API/$USERS")
     suspend fun getUserList(
         @Query(PAGE) page: Int
