e761fe50e587a4bbd6dde14cee3797cbcceb3ef3
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 02:50:05 +0900

    [FEAT] : data-remote 모듈 data source impl 구현

---
+++ data-remote/src/main/java/com/sopt/now/data_remote/datasourceimpl/AuthDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.sopt.now.data_remote.datasourceimpl
+
+import com.sopt.now.data.datasource.AuthDataSource
+import com.sopt.now.data.dto.request.RequestLoginDto
+import com.sopt.now.data.dto.request.RequestSignDto
+import com.sopt.now.data.dto.request.RequestUserPwDto
+import com.sopt.now.data.dto.response.ResponseLoginDto
+import com.sopt.now.data.dto.response.ResponseSignDto
+import com.sopt.now.data.dto.response.ResponseUserInfoDto
+import com.sopt.now.data.dto.response.ResponseUserPwDto
+import com.sopt.now.data_remote.api.AuthApiService
+import retrofit2.Response
+import javax.inject.Inject
+
+class AuthDataSourceImpl @Inject
+constructor(
+    private val authApiService: AuthApiService
+) : AuthDataSource {
+    override suspend fun postSign(requestSignDto: RequestSignDto): Response<ResponseSignDto> =
+        authApiService.postSign(requestSignDto)
+
+    override suspend fun postLogin(requestLoginDto: RequestLoginDto): Response<ResponseLoginDto> =
+        authApiService.postLogin(requestLoginDto)
+}
---
+++ data-remote/src/main/java/com/sopt/now/data_remote/datasourceimpl/HomeDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.sopt.now.data_remote.datasourceimpl
+
+import com.sopt.now.data.datasource.HomeDataSource
+import com.sopt.now.data.dto.request.RequestUserPwDto
+import com.sopt.now.data.dto.response.ResponseUserInfoDto
+import com.sopt.now.data.dto.response.ResponseUserPwDto
+import com.sopt.now.data_remote.api.AuthApiService
+import javax.inject.Inject
+
+class HomeDataSourceImpl @Inject
+constructor(
+    private val authApiService: AuthApiService
+): HomeDataSource {
+    override suspend fun getUserInfo(memberId: Int): ResponseUserInfoDto =
+        authApiService.getUserInfo(memberId)
+
+    override suspend fun patchUserPw(
+        memberId: Int,
+        requestUserPwDto: RequestUserPwDto
+    ): ResponseUserPwDto = authApiService.patchUserPw(memberId, requestUserPwDto)
+}
