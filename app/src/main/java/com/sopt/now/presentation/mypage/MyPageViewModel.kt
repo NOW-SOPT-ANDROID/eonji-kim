8064903db38d778ec8981d042b626860a9df7229
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 03:16:36 +0900

    [MOD] : liveData -> flow 변경

--- app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/mypage/MyPageViewModel.kt
@@ -1,6 +1,5 @@
 package com.sopt.now.presentation.mypage
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.core.MainApplication
@@ -10,7 +9,9 @@ import com.sopt.now.data.dto.request.RequestUserPwDto
 import com.sopt.now.data.dto.response.ResponseUserInfoDto
 import com.sopt.now.data.dto.response.ResponseUserPwDto
 import com.sopt.now.presentation.util.Regex.PW_REGEX
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
@@ -19,8 +20,8 @@ class MyPageViewModel : ViewModel() {
     private val _getUserInfo = MutableStateFlow<UiState<ResponseUserInfoDto>>(UiState.Loading)
     val getUserInfo: StateFlow<UiState<ResponseUserInfoDto>> = _getUserInfo
 
-    private val _patchUserPw = MutableLiveData<UiState<ResponseUserPwDto>>(UiState.Loading)
-    val patchUserPw: MutableLiveData<UiState<ResponseUserPwDto>> = _patchUserPw
+    private val _patchUserPw = MutableSharedFlow<UiState<ResponseUserPwDto>>()
+    val patchUserPw: SharedFlow<UiState<ResponseUserPwDto>> = _patchUserPw
 
     init {
         getUserInfo()
@@ -34,6 +35,8 @@ class MyPageViewModel : ViewModel() {
     }
 
     fun patchUserPw(beforePw: String, afterPw: String, checkPw: String) = viewModelScope.launch {
+        _patchUserPw.emit(UiState.Loading)
+
         if (checkPwValid(afterPw)) {
             runCatching {
                 ServicePool.authServiceApi.patchUserPw(
@@ -41,12 +44,13 @@ class MyPageViewModel : ViewModel() {
                     RequestUserPwDto(beforePw, afterPw, checkPw)
                 )
             }.fold(
-                { _patchUserPw.value = UiState.Success(it) },
-                { _patchUserPw.value = UiState.Failure(it.message.toString()) }
+                { _patchUserPw.emit(UiState.Success(it)) },
+                { _patchUserPw.emit(UiState.Failure(it.message.toString())) }
             )
         } else {
-            _patchUserPw.value =
+            _patchUserPw.emit(
                 UiState.Failure("비밀번호를 8글자 이상, 숫자, 문자(a-z, A-Z), 특수문자 포함하여 입력해주세요.")
+            )
         }
     }
 
