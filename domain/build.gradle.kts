caec6c75f1a9f9bd224b30b5f2b0a4dbec5c1c41
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 02:47:12 +0900

    [FEAT] : domain 모듈 build gradle 구현

---
+++ domain/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java-library")
+    kotlin("jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.kotlin.coroutines)
+}
