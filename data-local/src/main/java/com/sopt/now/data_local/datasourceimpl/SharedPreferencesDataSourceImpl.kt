ff61756781df6ac52aee4053ba8403e79215f30c
Eonji-sw <ejkim0625@gmail.com>
2024-05-29 02:49:27 +0900

    [FEAT] : data-local 모듈 data source impl 구현

---
+++ data-local/src/main/java/com/sopt/now/data_local/datasourceimpl/SharedPreferencesDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.sopt.now.data_local.datasourceimpl
+
+import android.content.SharedPreferences
+import com.sopt.now.data.datasource.SharedPreferenceDataSource
+import javax.inject.Inject
+import androidx.core.content.edit
+
+class SharedPreferencesDataSourceImpl @Inject constructor(
+    private val sharedPreferences: SharedPreferences,
+) : SharedPreferenceDataSource {
+    override var memberId: Int
+        get() = sharedPreferences.getInt(MEMBER_ID, -1)
+        set(value) = sharedPreferences.edit { putInt(MEMBER_ID, value) }
+
+    companion object {
+        const val MEMBER_ID = "member_id"
+    }
+}
