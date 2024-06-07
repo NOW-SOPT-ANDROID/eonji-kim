52c19a9a59c3083106131c23e957eb78ba6407b5
Eonji-sw <ejkim0625@gmail.com>
2024-06-07 18:31:44 +0900

    [MOD] : 패키지 위치 변경

--- app/src/main/java/com/sopt/now/compose/feature/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/feature/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.sopt.now.compose.presentation
+package com.sopt.now.compose.feature
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.sopt.now.compose.presentation.login.LoginActivity
+import com.sopt.now.compose.feature.login.LoginActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
